18706a5dc88e82a2b3ec3e4395b0b8f257b8bb2b
harisalimughal <harisalimughal077@gmail.com>
2025-04-22 10:44:09 +0500

    Removed temporary console statement

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async ()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected:${conn.connection.host} `)
-        console.log("MONGO_URI from env:", process.env.MONGO_URI); // TEMP debug
 
 
         
